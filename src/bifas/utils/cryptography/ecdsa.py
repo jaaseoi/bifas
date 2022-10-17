1f01cd7eecaacd0e7954a7be7bab7e79d18d34d5
Mike <u3568882@connect.hku.hk>
2022-10-18 07:06:02 +0800

    Edited and tested ECDSA

--- src/bifas/utils/cryptography/ecdsa.py
+++ src/bifas/utils/cryptography/ecdsa.py
@@ -1,62 +1,110 @@
-# Stateless function of generating private key from arbitrary passphrase, return as utils.data_structure.binary.Binary.
-# Stateless function of generating public key from private key, return as utils.data_structure.binary.Binary.
-# Stateless function of generating account address from public key, return as utils.data_structure.binary.Binary.
-# Stateless function of digital signature from private key and message (utils.data_structure.binary.Binary), return as utils.data_structure.binary.Binary.
-# Stateless boolean function to verify digital signed message (utils.data_structure.binary.Binary) with public key (utils.data_structure.binary.Binary).
-
 from Crypto.PublicKey import ECC
 from Crypto.Hash import SHA256
-from Crypto.Hash import RIPEMD160
-from Crypto.Signature import eddsa
+from Crypto.Signature import DSS
 from bifas.utils.data_structure.binary import Binary
+import os
 
-class ECDSAKeys:
+MODE = "fips-186-3"
 
-    def __init__(self, passphrase=None) -> None:
-        self.private_key, self.public_key = self.generate_key_pairs(passphrase)
-        self.address = self.generate_address(self.public_key)
-        pass
+class ECDSA:
+    """
+    DSA and ECDSA are U.S. federal standards for digital signatures, specified in FIPS PUB 186-4.
 
-    def generate_key_pairs(self, passphrase_binary:Binary) -> Binary:
-
-        passphraseIsBinary = type(passphrase_binary) == Binary
-
-        if passphraseIsBinary:
-            passphrase = passphrase_binary.get_x(data_format="bytes")
-            sha256_hash = SHA256.new(passphrase).hexdigest()
-            sha256_hash_int = int(sha256_hash, 16)
-            ecc_keys = ECC.construct(curve='P-256', d=sha256_hash_int)
+    Parameters
+    ----------
+    passphrase:Binary, default None,
+        If passphrase == None, random generate 256 bit string.
+    Examples
+    --------
+    >>> ecdsa = ECDSA(
+        passphrase=Binary(
+            x="BIFAS",
+            data_format="utf-8",
+        )
+    )
+    >>> self.assertEqual(b"\x42\x49\x46\x41\x53", ecdsa.passphrase.get_x(data_format="bytes"))
+    True
+    >>> message = Binary(
+        x="This is the receipt. Blockchain International Financial Asset System",
+        data_format="utf-8",
+    )
+    >>> message = Binary(
+        x="This is the receipt. Blockchain International Financial Asset System",
+        data_format="utf-8",
+    )
+    >>> self.assertEqual(
+        ecdsa.verify(
+            public_key=ecdsa.public_key,
+            message=message,
+            signature=ecdsa.sign(
+                message=message,
+            ),
+        ),
+        True,
+    )
+    True
+    """
+    def __init__(
+        self,
+        passphrase:Binary=None,
+    ) -> None:
+        self.passphrase, self.private_key, self.public_key, self.address = None, None, None, None
+        if ((passphrase == None) or (type(passphrase) != Binary)):
+            self.passphrase = Binary(
+                x=os.urandom(32),   # 256 bits
+                data_format="bytes",
+            )
         else:
-            ecc_keys = ECC.generate(curve='P-256')
-
-        private_key = ecc_keys.export_key(format='DER')
-        public_key = ecc_keys.public_key().export_key(format='DER')
-
-        # private_key_binary = Binary(x=private_key, data_format='utf-8')
-        return private_key, public_key
-
-    def generate_address(self, public_key:Binary) -> Binary:
+            self.passphrase = passphrase
+        __ecc_keys = ECC.construct(
+            curve="P-256",
+            d=int(
+                SHA256.new(
+                    data=self.passphrase.get_x(
+                        data_format="bytes",
+                    ),
+                ).hexdigest(),
+                base=16,
+            ),
+        )
+        self.private_key = Binary(
+            x=__ecc_keys.export_key(format="PEM"),
+            data_format="ascii",
+        )
+        self.public_key = Binary(
+            x=__ecc_keys.public_key().export_key(format="PEM"),
+            data_format="ascii",
+        )
         pass
 
-    def sign(self, private_key:Binary, message:Binary) -> Binary:
-        key = ECC.import_private_key(private_key)
-        signer = eddsa.new(key, mode='rfc8032')
-        signature = eddsa.sign(message)
-        return signature
+    def sign(
+        self,
+        message:Binary=None,
+    ) -> Binary:
+        return Binary(
+            DSS.new(
+                key=ECC.import_key(self.private_key.get_x(data_format="ascii")),
+                mode=MODE,
+            ).sign(
+                msg_hash=SHA256.new(message.get_x(data_format="bytes")),
+            ),
+            data_format="bytes",
+        )
 
-    def verify_signature(self, public_key:Binary, message:Binary, signature:Binary) -> bool:
-        key = ECC.import_public_key(public_key)
-        verifier = eddsa.new(key, mode='rfc8032')
+    def verify(
+        self,
+        public_key:Binary,
+        message:Binary,
+        signature:Binary
+    ) -> bool:
         try:
-            verifier.verify(message, signature)
+            DSS.new(
+                key=ECC.import_key(public_key.get_x(data_format="ascii")),
+                mode=MODE,
+            ).verify(
+                msg_hash=SHA256.new(message.get_x(data_format="bytes")),
+                signature=signature.get_x(data_format="bytes"),
+            )
             return True
         except ValueError:
             return False
-
-
-# Quick test
-passphrase = b"For instance, lets say you were planning to build a cheap MAC by concatenating a secret key to a public message m (bad idea!"
-keys = ECDSAKeys(passphrase)
-print(type(keys.private_key))
-print(keys.public_key)
-# keys.sign(keys.private_key, "Hello World!")
